5cdca176c60d8858be9faa1657b87436e8b6931f
Saqlain Ali Javed <saqlain.pucit@gmail.com>
2024-11-13 18:13:11 +0500

    update packages

--- src/theme/header/HeaderContent.js
+++ src/theme/header/HeaderContent.js
@@ -1,7 +1,4 @@
 import { memo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setSidebarCompact } from 'store/reducers/themeOptionsSlice';
-import IconButton from '@mui/material/IconButton';
 import { Stack } from '@mui/material';
 import UserMenu from './UserMenu';
 import HeaderLogo from './HeaderLogo';
