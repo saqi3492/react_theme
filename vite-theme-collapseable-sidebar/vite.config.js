9c2f9e609ba2a3281f23bed7e628db0b187ff4a9
Saqlain Ali Javed <saqlain.pucit@gmail.com>
2025-02-27 18:53:10 +0500

    initial vite setup

---
+++ vite-theme-collapseable-sidebar/vite.config.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+
+const __dirname = path.dirname('./');
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), eslintPlugin({ failOnError: true, failOnWarning: false })],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+});
