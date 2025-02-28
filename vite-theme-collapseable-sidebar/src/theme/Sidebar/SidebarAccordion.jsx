8640c00a4161995d75c5f9f97533a8bd8d38e2ac
Saqlain Ali Javed <saqlain.pucit@gmail.com>
2025-02-28 18:29:57 +0500

    fix warning

--- vite-theme-collapseable-sidebar/src/theme/Sidebar/SidebarAccordion.jsx
+++ vite-theme-collapseable-sidebar/src/theme/Sidebar/SidebarAccordion.jsx
@@ -117,6 +117,7 @@ const SidebarAccordion = props => {
         ref={elementRef}
         className="expansion-panel"
         style={{
+          // eslint-disable-next-line react-compiler/react-compiler
           maxHeight: !collapsed || sidebarCompact ? '0px' : componentHeight.current + 'px',
         }}
       >
