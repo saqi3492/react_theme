0df217ab80e9103d2d045508b9c339b783e75b3a
Saqlain Ali Javed <saqlain.pucit@gmail.com>
2025-05-29 14:29:57 +0500

    test commit

--- vite-theme-collapseable-sidebar/src/theme/Sidebar/SidebarAccordion.jsx
+++ vite-theme-collapseable-sidebar/src/theme/Sidebar/SidebarAccordion.jsx
@@ -45,6 +45,18 @@ const ItemText = styled(Span)(({ theme, compact, active }) => ({
 
 const SidebarAccordion = props => {
   const { item, children, sidebarCompact } = props;
+
+
+
+
+
+
+
+
+
+
+
+  
   const elementRef = useRef(null);
   const componentHeight = useRef(0);
   const { pathname } = useLocation();
@@ -90,7 +102,7 @@ const SidebarAccordion = props => {
       setHasActive(false);
     };
   }, [calcaulateHeight, pathname, item.children]);
-  
+
   return (
     <NavExpandRoot className="subMenu">
       <NavItemButton sx={{ padding: '0 12px', justifyContent: 'space-between' }} onClick={handleClick} active={hasActive ? 1 : 0}>
