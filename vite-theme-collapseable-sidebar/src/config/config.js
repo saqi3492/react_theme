5458ee66d71facea7dba18d72f6ca9f3569ff2c2
Abdul Basit Ali <67542561+bstali@users.noreply.github.com>
2025-07-02 20:33:15 +0500

    removed moment and used dayjs instead of it (#9)

--- vite-theme-collapseable-sidebar/src/config/config.js
+++ vite-theme-collapseable-sidebar/src/config/config.js
@@ -1,4 +1,6 @@
 export const config = {
   frontendUrl: import.meta.env.VITE_FRONTEND_URL,
   backendUrl: import.meta.env.VITE_BACKEND_URL,
+  dateFormat: 'MM-DD-YYYY',
+  timeFormat: 'hh:mm a',
 };
