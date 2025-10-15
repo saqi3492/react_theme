a69176d9007c4bb4d44ff31996998b687e9887ba
Saqlain Ali Javed <saqlain.pucit@gmail.com>
2025-10-15 16:07:05 +0500

    test commit

--- shad-cn-tailwind-theme/src/App.tsx
+++ shad-cn-tailwind-theme/src/App.tsx
@@ -1,6 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
-import ProgressAlerts from './components/layout/loader/ProgressAlerts';
 import Routes from './routes/routes';
+import ProgressAlerts from './components/layout/loader/ProgressAlerts';
+
+
+
+
+
+
+
 
 const ThemeWrapper = () => {
   return (
