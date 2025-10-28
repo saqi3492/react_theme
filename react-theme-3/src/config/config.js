7a9ec165e25f09c4cd0a0ec788be248ca073916d
Saqlain Ali Javed <67510524+saqi3492@users.noreply.github.com>
2025-10-28 20:19:47 +0500

    delete CRA themes (#17)

--- react-theme-3/src/config/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-  frontendUrl: process.env.REACT_APP_FRONTEND_URL,
-  backendUrl: process.env.REACT_APP_BACKEND_URL,
-};
--- react-theme-3/src/pages/incidents/Incidents.js
+++
@@ -1,5 +0,0 @@
-const Incidents = () => {
-  return <div>Incidents</div>;
-};
-
-export default Incidents;
